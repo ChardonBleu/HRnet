608242db8ec672676c309fc3742f506ec8410f54
ChardonBleu <f.rondi@orange.fr>
2025-08-13 14:02:44 +0200

    🧪 test: install and configure vitest

---
+++ app/components/Header/Header.test.tsx
@@ -0,0 +1,9 @@
+import { describe, it, expect, vi } from 'vitest'
+import { render, screen, fireEvent } from '@testing-library/react'
+
+describe('Header', () => {
+    it('', () => {
+        const example = "example"
+        expect(example).toEqual("example")
+    })
+})
