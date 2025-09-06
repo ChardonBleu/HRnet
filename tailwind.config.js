5cfd7cf8f12bde90d49f3c5db250cf598140984d
ChardonBleu <f.rondi@orange.fr>
2025-09-06 16:49:45 +0200

    🔧 config: configure for built app preview

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./app/**/*.{js,ts,jsx,tsx,mdx}", "./index.html"],
+  theme: {
+    // Laissez vide pour que vos @theme CSS fonctionnent
+  },
+  plugins: [],
+};
